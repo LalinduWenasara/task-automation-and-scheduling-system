f5d091195e33b7822473f19f2b039c641ffc0184
LalinduWenasara <49421049+LalinduWenasara@users.noreply.github.com>
2025-12-06 19:13:27 +0530

    quality gate trigger

--- task-automation-and-scheduling-system/src/main/java/com/adl/dc/ep/taskautomation/task_automation_and_scheduling_system/dto/TaskRequest.java
+++ task-automation-and-scheduling-system/src/main/java/com/adl/dc/ep/taskautomation/task_automation_and_scheduling_system/dto/TaskRequest.java
@@ -2,4 +2,12 @@ package com.adl.dc.ep.taskautomation.task_automation_and_scheduling_system.dto;
 
 public class TaskRequest {
     public Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
