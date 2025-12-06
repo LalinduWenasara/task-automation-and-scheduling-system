5d20ed00c1b77cd4ded305d5ffeca3e6870581e3
LalinduWenasara <49421049+LalinduWenasara@users.noreply.github.com>
2025-12-06 18:01:41 +0530

    update

--- task-automation-and-scheduling-system/src/main/java/com/adl/dc/ep/taskautomation/task_automation_and_scheduling_system/config/SecurityConfig.java
+++ task-automation-and-scheduling-system/src/main/java/com/adl/dc/ep/taskautomation/task_automation_and_scheduling_system/config/SecurityConfig.java
@@ -48,13 +48,22 @@ public class SecurityConfig {
         return http.build();
     }
 
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService);
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//        return authProvider;
+//    }
+
     @Bean
     public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
+        //
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
--- task-automation-and-scheduling-system/src/main/java/com/adl/dc/ep/taskautomation/task_automation_and_scheduling_system/security/JwtAuthenticationFilter.java
+++ task-automation-and-scheduling-system/src/main/java/com/adl/dc/ep/taskautomation/task_automation_and_scheduling_system/security/JwtAuthenticationFilter.java
@@ -15,7 +15,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    
+
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
