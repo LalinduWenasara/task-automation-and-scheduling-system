1da975b752761ac7216494a4e4553c4c2294bc46
LalinduWenasara <49421049+LalinduWenasara@users.noreply.github.com>
2025-12-14 16:22:20 +0530

    sonar issue fix

--- task-automation-and-scheduling-system/src/main/java/com/adl/dc/ep/taskautomation/task_automation_and_scheduling_system/service/EmailService.java
+++ task-automation-and-scheduling-system/src/main/java/com/adl/dc/ep/taskautomation/task_automation_and_scheduling_system/service/EmailService.java
@@ -1,8 +1,6 @@
 package com.adl.dc.ep.taskautomation.task_automation_and_scheduling_system.service;
 
 import com.adl.dc.ep.taskautomation.task_automation_and_scheduling_system.domain.Task;
-import com.adl.dc.ep.taskautomation.task_automation_and_scheduling_system.dto.AuthResponse;
-import com.adl.dc.ep.taskautomation.task_automation_and_scheduling_system.dto.LoginRequest;
 
 public interface EmailService {
 
